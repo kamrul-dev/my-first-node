72d5209eff598326835a3efeb19a2d586a6e6c22
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-19 20:53:01 +0600

    server connection req express with node

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port =process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('I can Code Node Now')
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
