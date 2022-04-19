be552f7fc720e1dafd12a0f031db825b9c8ecdfd
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-19 21:36:11 +0600

    Create dynamic api, api parameter, access params

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('I can Code Node Now Yayyyy!')
 });
 
+const users = [
+    { id: 1, name: 'Kamrul', email: 'kamrulprl@gmail.com', phone: '215653621' },
+    { id: 2, name: 'Hasan', email: 'Hasan@gmail.com', phone: '215653621' },
+    { id: 3, name: 'Babu', email: 'Babu@gmail.com', phone: '215653621' },
+    { id: 4, name: 'Ratul', email: 'Ratul@gmail.com', phone: '215653621' },
+    { id: 5, name: 'Nayem', email: 'Nayem@gmail.com', phone: '215653621' },
+    { id: 6, name: 'Abdullah', email: 'Abdullah@gmail.com', phone: '215653621' },
+    { id: 7, name: 'Abuilla', email: 'Abuilla@gmail.com', phone: '215653621' },
+]
+
+app.get('/users', (req, res) => {
+    res.send(users)
+});
+
+app.get('/user/:id', (req, res) => {
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+    res.send(user)
+});
+
 app.listen(port, () => {
     console.log('Listening to port', port);
-})
+});
