b0c7846783676d9c9681ed559132ae5c0e4bb8c3
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-20 13:07:23 +0600

    note added

--- _steps.js
+++ _steps.js
@@ -1,5 +1,39 @@
 /* 
+    Basic Server Setup
+    -----------------------------------
     1. Create a folder manually or mkdir
     2. open command line to that folder
     3. npm init
+    4. npm install express
+    5. create index.js
+    -------------------------------------
+
+    For Express/node server
+    1. requuire express
+    2. create app varibale
+    3. decalare port
+    4. set app.get('/')
+    5. listen to port
+    6. node index.js
+    7. check your browser for that port
+
+    --------------------------------------
+
+    get
+
+
+
+    -----------------------------------
+    1. cors middleware
+
+
+
+
+    ----------------------------------
+
+
+
+    post
+    ---------------------------------
+    1.
 */
